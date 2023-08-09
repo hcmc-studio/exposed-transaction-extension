92bb5612942c6e176d74039eabc8c7e063db16bc
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-09 18:18:13 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,33 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("maven-publish")
 }
 
 group = "studio.hcmc"
-version = "1.0-SNAPSHOT"
+version = "0.0.4"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(project(":kotlin-coroutines-extension"))
-
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
+    maven { setUrl("https://jitpack.io") }
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "studio.hcmc"
+            artifactId = "exposed-transaction-extension"
+            version = "0.0.4"
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    implementation("com.github.hcmc-studio:kotlin-coroutines-extension:0.0.4-build-01")
+
+    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
 }
