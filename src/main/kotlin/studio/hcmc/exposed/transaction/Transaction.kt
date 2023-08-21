90777cda600934c759ca0da582e543b005fbd204
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-21 15:29:28 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/exposed/transaction/Transaction.kt
+++ src/main/kotlin/studio/hcmc/exposed/transaction/Transaction.kt
@@ -1,8 +1,11 @@
 package studio.hcmc.exposed.transaction
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.Connection
 import kotlin.coroutines.CoroutineContext
 
 fun <R> blockingTransaction(
@@ -10,12 +13,18 @@ fun <R> blockingTransaction(
     block: Transaction.() -> R
 ): R {
     return if (transaction == null) {
-        org.jetbrains.exposed.sql.transactions.transaction { block() }
+        transaction { block() }
     } else {
-        transaction.block()
+        transaction(transaction.transactionIsolation, transaction.readOnly, transaction.db) { transaction.block() }
     }
 }
 
+/**
+ * 트랜잭션 수행
+ * @param transaction 부모 [Transaction]
+ * @param context 이 트랜잭션을 수행할 대상 [CoroutineContext]
+ * @param block 트랜잭션에서 수행할 내용
+ */
 suspend fun <R> suspendedTransaction(
     transaction: Transaction? = null,
     context: CoroutineContext? = Dispatchers.Transaction,
@@ -24,6 +33,10 @@ suspend fun <R> suspendedTransaction(
     return if (transaction == null) {
         newSuspendedTransaction(context) { block() }
     } else {
-        transaction.block()
+        if (context != null) {
+            withContext(context) { transaction.block() }
+        } else {
+            transaction.block()
+        }
     }
 }
